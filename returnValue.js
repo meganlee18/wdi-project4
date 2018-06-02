218aec8a295b470e4fd839779800bef2b8c983e2
Megan Lee <wylee14@gmail.com>
2018-06-02 12:27:48 +1000

    added functionality that fetched  images and city details from api

--- returnValue.js
+++ returnValue.js
@@ -13,15 +13,36 @@ console.log(search)
 //convert to json object
 var urlQueryParams = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
 console.log(urlQueryParams)
+//var cityName = urlQueryParams.city
 
 //select the city within the object
 $cityName.append(urlQueryParams.city)
 
 //When citybutton is clicked
 $cityDetails.on("click", function(event) {
-  
+
   $aboutCountry.text('About the country')
-  $weather.text('Weather in')
+
+  var options = {
+    url: 'https://www.triposo.com/api/20180507/location.json',
+    data: {
+      id: urlQueryParams.city,
+      count:'3',
+      account: 'C995KGMT',
+      token: 'w1iffxcmvhlgj9y76d6ass468nxzt07l'
+    }
+  }
+
+  $.ajax(options).done(function (res) {
+    res.results.forEach(function (array) {
+      console.log(array.images[0].sizes.medium.url)
+      console.log(array.snippet)
+    })
+  })
+
+  //NEED TO APPEND TO BODY NEXT
+
+  $weather.text('Weather in the month of ' + (myDate.value.split("-")[1]))
   $currency.text('Currency Exchange (against AUD)')
 })
 
