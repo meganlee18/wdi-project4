1b1ff4157110de8db599c5792e94eea3f3a90caa
Megan Lee <wylee14@gmail.com>
2018-06-02 21:50:05 +1000

    finished city details functionality

--- returnValue.js
+++ returnValue.js
@@ -21,29 +21,41 @@ $cityName.append(urlQueryParams.city)
 //When citybutton is clicked
 $cityDetails.on("click", function(event) {
 
-  $aboutCountry.text('About the country')
+  //replaces spaces and special char in names, if any
+  var cityNoUnderscore = (urlQueryParams.city).split(' ').join('_')
+  var updatedCity = encodeURI(cityNoUnderscore).split("%").join('')
 
   var options = {
     url: 'https://www.triposo.com/api/20180507/location.json',
     data: {
-      id: urlQueryParams.city,
+      id: updatedCity,
       count:'3',
       account: 'C995KGMT',
       token: 'w1iffxcmvhlgj9y76d6ass468nxzt07l'
     }
   }
-
+  
   $.ajax(options).done(function (res) {
     res.results.forEach(function (array) {
-      console.log(array.images[0].sizes.medium.url)
-      console.log(array.snippet)
+      //console.log(array.images[0].sizes.medium.url)
+      //console.log(array.snippet)
+      
+      var $p = $('<p>')
+      
+      $p.append('<img src=' + array.images[0].sizes.medium.url + '>');
+      $aboutCountry.append($p);
+      $textContainer.append($aboutCountry);
+      $aboutCountry.text('About the city')
+      
+      var $p2 = $('<p>')
+      $p.append(array.snippet);
+      $aboutCountry.append($p);
+      $textContainer.append($aboutCountry);
     })
   })
 
-  //NEED TO APPEND TO BODY NEXT
-
-  $weather.text('Weather in the month of ' + (myDate.value.split("-")[1]))
-  $currency.text('Currency Exchange (against AUD)')
+  //$weather.text('Weather in the month of ' + (myDate.value.split("-")[1]))
+  //$currency.text('Currency Exchange (against AUD)')
 })
 
 
