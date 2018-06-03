8ebc1309f19644abb9062064d4fd3f81f83924c3
Megan Lee <wylee14@gmail.com>
2018-06-03 15:46:52 +1000

    refactored ajax functions

--- returnValue.js
+++ returnValue.js
@@ -20,76 +20,92 @@ $cityName.append(urlQueryParams.city)
 
 var currencyCode = ""
 
+var fetchCountryIdDoneHandler = function (res) {
+
+  res.results.forEach(function (array) {
+    //console.log(array.images[0].sizes.medium.url)
+    //console.log(array.snippet)
+    var countryName = array.country_id.toLowerCase()
+    console.log(countryName)
+
+    var $p = $('<p>')
+
+    $p.append('<img src=' + array.images[0].sizes.medium.url + '>');
+    $aboutCountry.append($p);
+    $textContainer.append($aboutCountry);
+    $aboutCountry.text('About the city')
+
+    var $p2 = $('<p>')
+    $p.append(array.snippet);
+    $aboutCountry.append($p);
+    $textContainer.append($aboutCountry);
+  })
+}
+
+var fetchCurrencyAjaxCall = function (data) {
+  var country = data.results[0].country_id.toLowerCase()
+  // .then() returns a new promise
+  return $.ajax({
+    url: 'https://restcountries.eu/rest/v2/name/' + country,
+  })
+}
+
+var fetchCurrencyDoneHandler = function (res) {
+  currencyCode = res[0].currencies[0].code
+  console.log(currencyCode)
+}
+
+var exchangeRateAjaxCall = function (data) {
+  return $.ajax({
+    url: 'https://free.currencyconverterapi.com/api/v5/convert',
+    data: {
+      q:'AUD_' + currencyCode, 
+      compact: 'y',
+    }
+  });
+}
+
+var exchangeRateDoneHandler = function (res) {
+  var rate = res['AUD_' + currencyCode].val
+  console.log(rate)
+  $currency.text('Exchange rate of 1 AUD to ' + currencyCode + ' :  ')
+  
+  var $p = $('<div style="color:orange;display:inline-block;">')
+
+  $p.append(rate);
+  $currency.append($p);
+  $textContainer.append($currency);
+
+  };
+
+  var encodedCity = function(cityName) {
+    //replaces spaces and special char in names, if any
+    var cityNoUnderscore = (cityName).split(' ').join('_')
+    return encodeURI(cityNoUnderscore).split("%").join('')
+  }
+
 //When citybutton is clicked
 $cityDetails.on("click", function (event) {
-
-  //replaces spaces and special char in names, if any
-  var cityNoUnderscore = (urlQueryParams.city).split(' ').join('_')
-  var updatedCity = encodeURI(cityNoUnderscore).split("%").join('')
 
   var fetchCountryId = $.ajax({
 
     url: 'https://www.triposo.com/api/20180507/location.json',
     data: {
-      id: updatedCity,
+      id: encodedCity(urlQueryParams.city),
       count: '3',
       account: 'C995KGMT',
       token: 'w1iffxcmvhlgj9y76d6ass468nxzt07l'
     }
-  }).done(function (res) {
+  }).done(fetchCountryIdDoneHandler);
 
-    res.results.forEach(function (array) {
-      //console.log(array.images[0].sizes.medium.url)
-      //console.log(array.snippet)
-      var countryName = array.country_id.toLowerCase()
-      console.log(countryName)
+  var fetchCurrency = fetchCountryId
+  .then(fetchCurrencyAjaxCall)
+  .done(fetchCurrencyDoneHandler);
 
-      var $p = $('<p>')
-
-      $p.append('<img src=' + array.images[0].sizes.medium.url + '>');
-      $aboutCountry.append($p);
-      $textContainer.append($aboutCountry);
-      $aboutCountry.text('About the city')
-
-      var $p2 = $('<p>')
-      $p.append(array.snippet);
-      $aboutCountry.append($p);
-      $textContainer.append($aboutCountry);
-    })
-  });
-
-  var fetchCurrency = fetchCountryId.then(function (data) {
-    var country = data.results[0].country_id.toLowerCase()
-    // .then() returns a new promise
-    return $.ajax({
-      url: 'https://restcountries.eu/rest/v2/name/' + country,
-    })
-  }).done(function (res) {
-    currencyCode = res[0].currencies[0].code
-    console.log(currencyCode)
-  })
-
-   var fetchExchangeRates = fetchCurrency.then(function (data) {
-    return $.ajax({
-      url: 'https://free.currencyconverterapi.com/api/v5/convert',
-      data: {
-        q:'AUD_' + currencyCode, 
-        compact: 'y',
-      }
-    });
-  }).done(function (res) {
-    var rate = res['AUD_' + currencyCode].val
-    console.log(rate)
-    $currency.text('Currency Exchange: 1 AUD to ' + currencyCode)
-    
-    var $p = $('<p>')
-
-    $p.append(rate);
-    $currency.append($p);
-    $textContainer.append($currency);
-
-    })
-  });
+   var fetchExchangeRates = fetchCurrency
+   .then(exchangeRateAjaxCall)
+   .done(exchangeRateDoneHandler);
+});
 
 
   //$weather.text('Weather in the month of ' + (myDate.value.split("-")[1]))
